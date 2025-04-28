73286ad86787c7eda3fdd9de8487ff7011754527
Paramveer Singh <paramveer7267@gmail.com>
2025-04-29 00:22:54 +0530

    finished backend

---
+++ backend/routes/user.route.js
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+  createTicket,
+  getUserTickets,
+  getAssignedTickets,
+  updateTicketStatus,
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+
+const router = express.Router();
+
+// Create a ticket (any user)
+router.post("/create", protectRoute, createTicket);
+
+// Get tickets for the logged-in user
+router.get("/user", protectRoute, getUserTickets);
+
+// for engineer assigned tickets
+router.get(
+  "/assigned-tickets",
+  protectRoute,
+  getAssignedTickets
+);
+
+// Update ticket status (admin & engineer only)
+router.patch(
+  "/:ticketId",
+  protectRoute,
+  updateTicketStatus
+);
+export default router;
+
+
